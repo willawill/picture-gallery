34113bc422b20bf3c88df206ac15ac6a14cfc594
Willa <mowmowjoker@gmail.com>
2014-03-24 12:24:51 -0400

    Put user's name on the home page.

--- src/picture_gallery/routes/home.clj
+++ src/picture_gallery/routes/home.clj
@@ -1,9 +1,15 @@
 (ns picture-gallery.routes.home
   (:require [compojure.core :refer :all]
-            [picture-gallery.views.layout :as layout]))
+            [noir.session :as session]
+            [picture-gallery.views.layout :as layout]
+            [picture-gallery.models.db :as db]))
+
+
+(defn get-user []
+  (session/get :user))
 
 (defn home []
-  (layout/common [:h1 "Hello World!"]))
+  (layout/common [:h1 "Hello " (get-user)]))
 
 (defroutes home-routes
   (GET "/" [] (home)))
