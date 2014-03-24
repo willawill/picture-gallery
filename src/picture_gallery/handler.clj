cfe64a715957819ea8fd28da9c0b1cbde0fa8ae8
Willa <mowmowjoker@gmail.com>
2014-03-24 11:29:30 -0400

    Adding dependencies and creating tables in database.

---
+++ src/picture_gallery/handler.clj
@@ -0,0 +1,19 @@
+(ns picture-gallery.handler
+  (:require [compojure.core :refer [defroutes routes]]
+            [compojure.route :as route]
+            [noir.util.middleware :as noir-middleware]
+            [picture-gallery.routes.home :refer [home-routes]]))
+
+(defn init []
+  (println "picture-gallery is starting"))
+
+(defn destroy []
+  (println "picture-gallery is shutting down"))
+
+(defroutes app-routes
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app (noir-middleware/app-handler [home-routes app-routes]))
+
+
